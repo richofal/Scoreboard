1551aebbe0b9782f3dbaf50e8124bd269bc88312
Richo Febrian <richo.febrian.alex-2023@fst.unair.ac.id>
2024-07-11 11:26:18 +0700

    Added reset functionality and dynamic max score selection

--- app.js
+++ app.js
@@ -2,28 +2,47 @@ const p1Button = document.querySelector("#p1-button");
 const p2Button = document.querySelector("#p2-button");
 const p1Display = document.querySelector("#p1-display");
 const p2Display = document.querySelector("#p2-display");
+const resetButton = document.querySelector("#reset");
+const maxPoint = document.querySelector("#selectMaxPoint");
 
 let p1Score = 0;
 let p2Score = 0;
-let winPoin = 5;
+let winPoin = parseInt(maxPoint.value);
 let isGameOver = false;
+
+function reset() {
+  isGameOver = false;
+  p1Score = 0;
+  p2Score = 0;
+  p1Display.textContent = p1Score;
+  p2Display.textContent = p2Score;
+}
 
 p1Button.addEventListener("click", function () {
   if (!isGameOver) {
     p1Score += 1;
-    if(p1Score === winPoin) {
-        isGameOver = true;
+    if (p1Score === winPoin) {
+      isGameOver = true;
     }
   }
   p1Display.textContent = p1Score;
 });
 
 p2Button.addEventListener("click", function () {
-    if (!isGameOver) {
-      p2Score += 1;
-      if(p2Score === winPoin) {
-          isGameOver = true;
-      }
+  if (!isGameOver) {
+    p2Score += 1;
+    if (p2Score === winPoin) {
+      isGameOver = true;
     }
-    p2Display.textContent = p2Score;
-  });
+  }
+  p2Display.textContent = p2Score;
+});
+
+resetButton.addEventListener("click", reset);
+
+maxPoint.addEventListener("change", max);
+
+function max() {
+  winPoin = parseInt(this.value);
+  reset();
+}
